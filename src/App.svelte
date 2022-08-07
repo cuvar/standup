91301e59e6f63604771b14131f12b5b8b0fe6f3a
cuvar <luca-mueller@gmx.net>
2022-08-07 14:10:30 +0200

    fix: set resizeable false

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Timer from "./lib/Timer.svelte";
   import { appWindow } from "@tauri-apps/api/window";
+  appWindow.setResizable(false);
+
   addEventListener("keydown", async (e) => {
     if (e.metaKey && e.key == "m") {
       await appWindow.minimize();
